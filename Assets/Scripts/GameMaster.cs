42c781b8460216e349c5e6da7d738c83d9941d15
嶋津恒彦 <simanezumi1989@gmail.com>
2020-10-09 14:52:47 +0900

    Create GameMaster

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+    // ゲームの進行を管理
+    //・手札の準備
+    //・両Playerがカードを提出したか確認
+    //・カードの勝敗を判定
+    //・カードの削除
+    //・次の対戦
+
+    [SerializeField] Player[] players;
+
+    private void Start()
+    {
+        foreach (Player player in players)
+        {
+            player.OnSubmitAction = OnSubmitCard;
+        }
+
+        CallSettingHand();
+    }
+
+    // 手札を準備させる
+    void CallSettingHand()
+    {
+        foreach (Player player in players)
+        {
+            player.SetupHand();
+        }
+    }
+
+    // Playerから提出通知を受ける
+
+    public void OnSubmitCard()
+    {
+        Debug.Log("Playerがカードを提出");
+    }
+
+    // 提出確認
+    bool IsCompletedSubmit()
+    {
+        return true;
+    }
+
+    // 判定
+    void Battle()
+    {
+
+    }
+
+    // 場のカードを捨てる
+    void NextSetting()
+    {
+
+    }
+}
